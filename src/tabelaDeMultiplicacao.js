76d9b94fd0608cc11f6f76e30c6b4263c7d195c9
Bluemoono <mikka_paulina@live.com>
2021-10-25 21:21:12 -0300

    função de tabuada pronto e anexisada ao index

--- src/tabelaDeMultiplicacao.js
+++ src/tabelaDeMultiplicacao.js
@@ -1,9 +1,15 @@
-function nomeDaFuncao() { 
+function tabelaMultiplicacao(n) { 
     // Substitua "nomeDaSuaFuncao" por um nome mais apropriado
     // TODO: escrever dois loops aninhados para desenhar o array bidimensional e monstar o restante da tabela no console.
     let x = [];
     for (let i = 0; i <= 10; i++) {
         x[i] = [];
+        for(let j=0; j<= 10; j++){
+            let mult = i*j
+            x[i].push(mult)
+        }
     }
     console.table(x)
 }
+
+tabelaMultiplicacao()
