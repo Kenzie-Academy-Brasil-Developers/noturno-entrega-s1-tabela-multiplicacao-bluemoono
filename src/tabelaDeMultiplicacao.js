edf15e33506932529bd6264a6aaf5fcdd5b00af1
Bluemoono <mikka_paulina@live.com>
2021-10-25 21:25:06 -0300

    Tabela extra

--- src/tabelaDeMultiplicacao.js
+++ src/tabelaDeMultiplicacao.js
@@ -1,15 +1,27 @@
 function tabelaMultiplicacao(n) { 
     // Substitua "nomeDaSuaFuncao" por um nome mais apropriado
     // TODO: escrever dois loops aninhados para desenhar o array bidimensional e monstar o restante da tabela no console.
-    let x = [];
-    for (let i = 0; i <= 10; i++) {
-        x[i] = [];
-        for(let j=0; j<= 10; j++){
-            let mult = i*j
-            x[i].push(mult)
+    if(n === undefined){
+        let x = [];
+        for (let i = 0; i <= 10; i++) {
+            x[i] = [];
+            for(let j=0; j<= 10; j++){
+                let mult = i*j
+                x[i].push(mult)
+            }
         }
+        console.table(x)
+    } else {
+        let x = [];
+        for (let i = 0; i <= n; i++) {
+            x[i] = [];
+            for(let j=0; j<= n; j++){
+                let mult = i*j
+                x[i].push(mult)
+            }
+        }
+        console.table(x)
     }
-    console.table(x)
 }
 
-tabelaMultiplicacao()
+tabelaMultiplicacao(11)
